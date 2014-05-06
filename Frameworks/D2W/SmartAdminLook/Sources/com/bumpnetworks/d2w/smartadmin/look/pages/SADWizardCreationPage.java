9cf397781d85ecbc7964961cffd748f4568ddd0c
Johnny Miller <jlmiller@kahalawai.com>
2014-05-05 18:20:18 -1000

    Started wizard creation page 	new file:   Frameworks/D2W/SmartAdminLook/Components/Nonlocalized.lproj/SADWizardCreationPage.wo/SADWizardCreationPage.html 	new file:   Frameworks/D2W/SmartAdminLook/Components/Nonlocalized.lproj/SADWizardCreationPage.wo/SADWizardCreationPage.wod 	new file:   Frameworks/D2W/SmartAdminLook/Components/Nonlocalized.lproj/SADWizardCreationPage.wo/SADWizardCreationPage.woo 	modified:   Frameworks/D2W/SmartAdminLook/Components/SADEditRelationshipPage.wo/SADEditRelationshipPage.html 	modified:   Frameworks/D2W/SmartAdminLook/Components/SADEditRelationshipPage.wo/SADEditRelationshipPage.wod 	new file:   Frameworks/D2W/SmartAdminLook/Components/SADWizardCreationPage.api 	modified:   Frameworks/D2W/SmartAdminLook/Resources/d2w.d2wmodel 	modified:   Frameworks/D2W/SmartAdminLook/Resources/d2w.d2wmodel.txt 	new file:   Frameworks/D2W/SmartAdminLook/Sources/com/bumpnetworks/d2w/smartadmin/look/pages/SADWizardCreationPage.java

---
+++ Frameworks/D2W/SmartAdminLook/Sources/com/bumpnetworks/d2w/smartadmin/look/pages/SADWizardCreationPage.java
@@ -0,0 +1,10 @@
+package com.bumpnetworks.d2w.smartadmin.look.pages;
+
+import com.webobjects.appserver.WOContext;
+import er.directtoweb.pages.templates.ERD2WWizardCreationPageTemplate;
+
+public class SADWizardCreationPage extends ERD2WWizardCreationPageTemplate {
+    public SADWizardCreationPage(WOContext context) {
+        super(context);
+    }
+}
